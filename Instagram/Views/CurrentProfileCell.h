142b804b28fe1e1ee4ccddb302a54d6d73bc48c4
Anvitha <anvithak23733@gmail.com>
2021-07-12 12:39:34 -0700

    Not currently using this cell in design

--- Instagram/Views/CurrentProfileCell.h
+++ Instagram/Views/CurrentProfileCell.h
@@ -10,6 +10,9 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+// This file is associated with an older design for the profile view, which included a table view to add a collection view and show only a user's posts on their page.
+// However, it is not currently in use since I had to cut this feature before the deadline.
+
 @interface CurrentProfileCell : UITableViewCell
 @property (strong, nonatomic) PFUser *user;
 @property (weak, nonatomic) IBOutlet UILabel *fullNameLabel;
