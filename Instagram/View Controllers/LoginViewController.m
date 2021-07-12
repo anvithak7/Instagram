ef82b36b2e62b2b838f51f5b0fce361ec4b6ad9b
Anvitha <anvithak23733@gmail.com>
2021-07-12 12:00:15 -0700

    Added comments to Login View Controller

--- Instagram/View Controllers/LoginViewController.m
+++ Instagram/View Controllers/LoginViewController.m
@@ -16,10 +16,14 @@
 
 @implementation LoginViewController
 
+// This view controller is for the user to be able to input their credentials and log in.
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
 }
+
+// The core functionality of this view controller. After fields are validated, Parse takes care of the login credentials, and if such a user exists, a user is directed to their home feed.
 - (IBAction)pressedLogin:(id)sender {
     [self validateFields];
     [PFUser logInWithUsernameInBackground:self.usernameField.text password:self.passwordField.text block:^(PFUser * user, NSError *  error) {
@@ -33,10 +37,13 @@
         }
         }];
 }
+
+// This allows a user to switch between the login and sign up screens.
 - (IBAction)pressedSignUp:(id)sender {
     [self performSegueWithIdentifier:@"SignUp" sender:nil];
 }
 
+// The below method checks for whether fields are blank, in which case an error would be thrown.
 - (BOOL) validateFields {
     if ([self.usernameField.text isEqual:@""]) {
         [self createAlert:@"Username cannot be blank. Please enter a valid username and try again!" error:@"Unable to Login"];
@@ -48,25 +55,22 @@
     return YES;
 }
 
+// A function to create alerts, instead of writing this out multiple times.
 - (void) createAlert: (NSString *)message error:(NSString*)error {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:error
-                                                                               message:message
-                                                                        preferredStyle:(UIAlertControllerStyleAlert)];
-
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:error message:message preferredStyle:(UIAlertControllerStyleAlert)];
     // create an OK action
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
-                                                       style:UIAlertActionStyleDefault
-                                                     handler:^(UIAlertAction * _Nonnull action) {
-                                                             // handle response here.
-                                                     }];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    // handle response here.
+    }];
     // add the OK action to the alert controller
     [alert addAction:okAction];
-    
     // show alert
     [self presentViewController:alert animated:YES completion:^{
         // optional code for what happens after the alert controller has finished presenting
     }];
 }
+
+// A user should be able to make the keyboard go away when they tap anywhere after they are done typing.
 - (IBAction)onTapAnywhere:(id)sender {
     [self.view endEditing:true];
 }
