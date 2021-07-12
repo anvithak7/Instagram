a97c9e911040d51c009aa613a8d43333e52d04fb
Anvitha <anvithak23733@gmail.com>
2021-07-12 12:05:24 -0700

    Added comments to Details View Controller

--- Instagram/View Controllers/DetailsViewController.m
+++ Instagram/View Controllers/DetailsViewController.m
@@ -21,9 +21,12 @@
 
 @implementation DetailsViewController
 
+// This view controller displays the information from a post on a separate screen when a post cell is tapped, and also allows a user to add likes.
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    // We want to set up the post's information - the image, username, user's profile picture, and caption.
     self.profilePictureView.layer.masksToBounds = YES;
     self.profilePictureView.layer.cornerRadius = self.profilePictureView.frame.size.width / 2;
     self.usernameLabel.text = _post.author[@"username"];
@@ -31,6 +34,7 @@
     [self.postImageView loadInBackground];
     self.profilePictureView.file = _post.author[@"profileImage"];
     [self.profilePictureView loadInBackground];
+    // The below lines allow the bolded username to be attached to the caption.
     NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:17.0]};
     NSMutableAttributedString *username = [[NSMutableAttributedString alloc] initWithString:_post.author[@"username"] attributes:attributes];
     NSMutableAttributedString *captionAlone = [[NSMutableAttributedString alloc] initWithString:_post[@"caption"]];
@@ -39,18 +43,22 @@
     [captionText appendAttributedString:[[NSMutableAttributedString alloc] initWithString:@" "]];
     [captionText appendAttributedString:captionAlone];
     self.captionLabel.attributedText = captionText;
+    // For the creation date and time, this details view provides more details about the exact time the posts were made.
     NSDate *createdDate = _post.createdAt;
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
     formatter.dateFormat = @"MMMM d, yyyy  h:mm a";
     self.timestampLabel.text = [formatter stringFromDate:createdDate];
+    // This sets up what the like button should look like, in case the user has already liked this post.
     if ([self.post.likesArray containsObject:PFUser.currentUser.objectId]) {
         [self.likeButton setSelected:YES];
     } else if (![self.post.likesArray containsObject:PFUser.currentUser.objectId]) {
         [self.likeButton setSelected:NO];
     }
+    // Finally, the number of likes a post has should be displayed in the details view.
     self.likeCountLabel.text = [NSString stringWithFormat:@"%@", self.post[@"likeCount"]];
 }
 
+// This is similar to the method associated with PostCell, where a user can click on the like button to like a post and send that information to the database.
 - (IBAction)onTapLike:(id)sender {
     if ([self.post.likesArray containsObject:PFUser.currentUser.objectId]) {
         [self.likeButton setSelected:NO];
